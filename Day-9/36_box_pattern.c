d516490dc7f9574828071139ae4a850a37076554
Navya Singh <navya102007@gmail.com>
2026-06-05 10:40:38 +0530

    Add files via upload

---
+++ Day-9/36_box_pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int r, c;
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+    for(int i=1; i<=r; i++)
+    {
+        for(int j=1; j<=c; j++)
+        {
+            if(j==1 || j==c || i==1 || i==r)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
