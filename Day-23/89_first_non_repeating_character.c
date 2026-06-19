300ee2787d05e37d1c48f52fa72b26ad9d90f706
Navya Singh <navya102007@gmail.com>
2026-06-19 09:19:25 +0530

    Create 89_first_non_repeating_character.c

---
+++ Day-23/89_first_non_repeating_character.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[100];
+    int freq[256]={0};
+    printf("Enter sentence: ");
+    scanf("%[^\n]", s);
+    for(int i=0; i<strlen(s); i++)
+    {
+        if((s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122))
+            freq[s[i]]++;
+    }
+    char ch;
+    for(int i=0; i<strlen(s); i++)
+    {
+        if(freq[s[i]]==1)
+        {
+            ch=s[i];
+            break;
+        }
+    }
+    printf("First Non-Repeating Character: %c", ch);
+    return 0;
+}
